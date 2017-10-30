745a994cf88c6d8d0c126354b63ff34b7945d09c
nhicof2 <nhicof2@nhicof2-desktop>
2017-10-30 00:50:13 -0300

    Comments ftw

--- Tablero.h
+++ Tablero.h
@@ -6,6 +6,9 @@
 
 class Tablero{
 
+	/*
+	 * Un tablero es
+	 */
 	private:
 
 		Casillero ** tablero;
@@ -15,42 +18,114 @@ class Tablero{
 
 	public:
 
+		/*
+		 * Pre :
+		 * Post:
+		 */
 		Tablero(uint filas,uint columnas);
 
+		/*
+		 * Pre :
+		 * Post:
+		 */
 		void inicializar(uint cantidadDeBombas);
 
+		/*
+		 * Pre :
+		 * Post:
+		 */
 		uint obtenerValorCasillero(uint fila,uint columna);
 
+		/*
+		 * Pre :
+		 * Post:
+		 */
 		uint obtenerEstadoCasillero(uint fila, uint columna);
 
+		/*
+		 * Pre :
+		 * Post:
+		 */
 		void cambiarEstadoCasillero(uint fila, uint columna, uint nuevoEstado);
 
+		/*
+		 * Pre :
+		 * Post:
+		 */
 		void marcarCasillero(uint fila, uint columna);
 
+		/*
+		 * Pre :
+		 * Post:
+		 */
 		void descubrirCasillero(uint fila, uint columna);
 
+		/*
+		 * Pre :
+		 * Post:
+		 */
 		uint obtenerCantidadFilas();
 
+		/*
+		 * Pre :
+		 * Post:
+		 */
 		uint obtenerCantidadColumnas();
 
+		/*
+		 * Pre :
+		 * Post:
+		 */
 		void descubrirTodasLasBombas();
 
+		/*
+		 * Pre :
+		 * Post:
+		 */
 		void imprimir();
 
+		/*
+		 * Pre :
+		 * Post:
+		 */
 		uint casillerosRestantesPorDescubrir();
 
+		/*
+		 * Pre :
+		 * Post:
+		 */
 		~Tablero();
 
 	private:
 
+		/*
+		 * Pre :
+		 * Post:
+		 */
 		void ponerBombasAleatoriamente(uint cantidadBombas);
 
+		/*
+		 * Pre :
+		 * Post:
+		 */
 		void rellenarConNumeros();
 
+		/*
+		 * Pre :
+		 * Post:
+		 */
 		void rellenarSubMatrizAledaniaBomba(int fila,int columna);
 
+		/*
+		 * Pre :
+		 * Post:
+		 */
 		int descubrirCasillerosAledaniosVacio(int fila,int columna);
 
+		/*
+		 * Pre :
+		 * Post:
+		 */
 		bool esPosicionValida(uint fila, uint columna);
 
 
